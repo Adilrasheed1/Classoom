c4aba868933f46ec35f3590d807232450160d2b3
Adilrasheed1 <adilrasheed225@gmail.com>
2026-05-03 23:40:31 +0530

    added send money componenet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import { UserDashboard } from "./pages/UserDashboard"
 import SignupPage from "./pages/SignupPage"
 import SigninPage from "./pages/SigninPage"
+import { SendMoney } from "./pages/SendMoney"
 function App() {
  
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="userDashboard" element={<UserDashboard/>}/>
         <Route path="signup" element={<SignupPage/>}/>
          <Route path="signin" element={<SigninPage/>}/>
-
+         <Route path="send" element={<SendMoney/>}/>
         
       </Routes>
       </BrowserRouter>
--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,11 +1,12 @@
-
+import { useNavigate } from "react-router-dom"
 
 export function User({user}){
-    
+   const navigate=useNavigate() 
     return <div>
         <div className="flex flex-row justify-between w-180  ml-5 mt-2 mb-2  pl-3 rounded-sm border-1 border-gray-300 bg-gray-200">
             <span><h3>{user.firstname} {user.lastname}</h3></span>
-            <button className="bg-blue-400 w-20 rounded-md text-gray-200">Send Money</button>
+            <button onClick={(e) => {
+                navigate("/send?id=" + user._id + "&name=" + user.firstname)}} className="bg-blue-400 w-20 rounded-md text-gray-200">Send Money</button>
         </div>
     </div>
 }
